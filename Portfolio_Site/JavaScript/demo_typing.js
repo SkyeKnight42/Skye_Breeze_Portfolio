da2769c905b2c4040733264b0e7d4662fa4cb75e
Skye Breeze <skyexwarren@gmail.com>
2021-02-15 15:17:49 +0000

    JavaScript Fixes

--- Portfolio_Site/JavaScript/demo_typing.js
+++ Portfolio_Site/JavaScript/demo_typing.js
@@ -1,24 +1,7 @@
 
+let textToType = document.getElementById("typing_effect")
+let button = document.getElementById("demo_button")
 
-let text= document.getElementById('typing_effect')
-let textLength = text.textContent.length
-let button = document.getElementById('demo_button')
-let textcopy
-
-
-button.addEventListener('click', function() {
-    if (text.textContent.length == textLength) {
-        textcopy = text.textContent
-        text.innerHTML = ""
-    
-        for (let x = 0; x < textcopy.length; x++) {
-            addLetter(x, textcopy.charAt(x))
-        }
-    }
+button.addEventListener("click", function() {
+    TypeOutText(textToType, 50, "typing", null)
 })
-
-function addLetter(_index, _letter) {
-    setTimeout(function() {
-        text.innerHTML += _letter
-    }, _index * 50)
-}
